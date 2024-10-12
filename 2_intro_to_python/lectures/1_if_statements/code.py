17c0c3c11713c4f2436c14e731302638ddf221ef
Moiz-stack <moiz.workmail@gmail.com>
2024-10-12 02:12:24 -0700

    change

--- 2_intro_to_python/lectures/1_if_statements/code.py
+++ 2_intro_to_python/lectures/1_if_statements/code.py
@@ -1,4 +1,5 @@
-friend = "Rolf"
+# Initial greeting
+friend = "Moiz"
 user_name = input("Enter your name: ")
 
 if user_name == friend:
@@ -6,14 +7,14 @@ if user_name == friend:
 else:
     print("Hello, stranger!")
 
-
 # -- Checking whether the if statement will run --
 
 print(bool(user_name == friend))  # if this is True, the if statement will run
 
 # -- Using the `in` keyword --
 
-friends = ["Rolf", "Bob", "Anne"]
+# Updated list of friends and family
+friends = ["Moiz", "Sharjeel", "Nauman", "Ali Sheikh"]
 family = ["Jen", "Charlie"]
 
 user_name = input("Enter your name: ")
